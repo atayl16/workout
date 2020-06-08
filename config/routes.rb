6d551e153e4c570bbc4b1aaeb3fc1a1508688f54
Alisha <atayl16@yahoo.com>
2020-06-09 00:58:53 +0100

    Final routes config to close project

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ require 'subdomain'
 Rails.application.routes.draw do
 
   resources :exercises
+  class Subdomain
+    def self.matches?(request)
+      subdomains %w{ www admin } # reserved subdomains
+      request.subdomain.present? && !subdomains.include?(request.subdomain)
+    end
+  end
+
   constraints Subdomain do
     resources :works
   end
